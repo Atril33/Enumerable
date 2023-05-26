108c742e24ce82130f08ead759affb2885b080a2
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-05-26 13:22:57 +0500

    Fixed linters issues

--- my_list.rb
+++ my_list.rb
@@ -7,11 +7,6 @@ class MyList
     @list = items
   end
 
-  def each
-    @list.each { |ele| yield ele }
-  end
-end
-
 list = MyList.new(1, 2, 3, 4)
 list.all? { |e| e > 5 }
 list.any? { |e| e == 5 }
